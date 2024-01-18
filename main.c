a434d9fba0d99f60dd4181cdb162c82af8f08cd8
nathen321 <walidjalaly43@gmail.com>
2024-01-18 13:41:32 -0800

    ok

---
+++ main.c
@@ -0,0 +1,53 @@
+#include "monty.h"
+/**
+ * main - ...........
+ *
+ * Description: ........
+ */
+int main(int argc,char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	open_file(argv[1]);
+	free_list_stack();
+	return (0);
+}
+/**
+ * create_node - Creates a node.
+ * @n: Number to go inside the node.
+ * Return: Upon sucess a pointer to the node. Otherwise NULL.
+ */
+stack_t *create_node(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+
+/**
+ * free_list_stack - Frees nodes in the stack.
+ */
+void free_list_stack(void)
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
